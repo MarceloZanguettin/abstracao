3bba8d5ea20a8dd69a4a7f290e25d2ed31715797
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-05-30 19:37:32 +0000

    Exemplo

---
+++ Indexacao.java
@@ -0,0 +1,27 @@
+public class Indexacao {
+//fazer uma referencia de palavras com o texto relacionado
+
+    private String indice;
+
+    public String getIndice() {
+        return indice;
+    }
+
+    public void setIndice(String indice) {
+        this.indice = indice;
+    }
+
+    public String abstrairTexto(String texto){
+        String resposta=null;
+        if(texto.equalsIgnoreCase("c")){
+            resposta="C a linguagem estruturada";
+        }
+        else if(texto.equalsIgnoreCase("java")){
+            resposta="Java Orientação a objetos";
+        }
+        else if(texto.equalsIgnoreCase("python")){
+            resposta="Python a linguagem de data Science";
+        }
+        return resposta;
+    }
+}
